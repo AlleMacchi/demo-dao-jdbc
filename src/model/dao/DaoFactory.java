fc18b9296ea249c289887381cc86f4b810217ed6
AlleMacchi <alessandro.macchitella@gmail.com>
2020-04-26 21:46:36 +0200

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createDao() {		
-		SellerDaoJDBC obj = new SellerDaoJDBC();
+		SellerDaoJDBC obj = new SellerDaoJDBC(DB.getConnection());
 		return obj;
 	};
 
